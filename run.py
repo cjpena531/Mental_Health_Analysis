b7b7510ac12f7718388f984a65e1a96e6b9acec7
myhaider <myhaider@myhaider-22445.pods.myhaider.svc.cluster.local>
2020-12-02 22:38:29 -0800

    clean target

--- run.py
+++ run.py
@@ -1,25 +1,30 @@
 import os 
 import json
 import sys
-sys.path.insert(0,sys.path[0] +'/src')
+sys.path.append('src')
 from etl import *
 
 def main():
     
     args = sys.argv
-    print(args)
     
     if (len(args) > 1):
         if (args[1] == "test"):
-            print("yes")
+            print("testing...")
             dictionary = json.load(open("config/test_parameters.json"))
+        elif (args[1] == "clean"):
+            print("cleaning...")
+            clean()
+            print("cleaned!")
+            return
     else:
+        print(os.listdir('/datasets'))
         dictionary = json.load(open("config/parameters.json"))
     
-    
+    create_folders()
     subset = get_subset(dictionary['read_1'],dictionary['read_2'],dictionary['subset_size'])
     fastqc(dictionary,subset)
-    cutadapt(dictionary,subset)
+    #cutadapt(dictionary,subset)
     kallisto(dictionary,subset)
     gene_counts(dictionary,subset)
     
--- src/etl.py
+++ src/etl.py
@@ -5,6 +5,22 @@ import numpy as np
 import pandas as pd
 
 
+def create_folders():
+    if not os.path.isdir('data'):
+        os.system('mkdir data')
+    if not os.path.isdir('data/fastqc_results'):
+        os.system('mkdir data/fastqc_results')
+    if not os.path.isdir('data/cutadapt'):
+        os.system('mkdir data/cutadapt')
+    if not os.path.isdir('data/kallisto'):
+        os.system('mkdir data/kallisto')
+    return
+
+def clean():
+    if os.path.isdir('data'):
+        os.system('rm -R data')
+    return
+
 def get_subset(read1, read2, size):
     r1 = glob.glob(read1)
     r2 = glob.glob(read2)
@@ -28,22 +44,19 @@ def fastqc(dictionary,subset):
 
         os.system(command1)
         os.system(command2)
+
+    zip_files = glob.glob(dictionary['fast_out_path']+"/*.zip")
     
-    os.chdir(dictionary['fast_out_path'])
-
-    if not os.path.isdir('unzipped'):
-        os.system('mkdir ' + 'unzipped')
-
-    os.system('unzip \*.zip -d unzipped')
-    os.chdir('..')
+    for file in zip_files:
+        os.system(f"unzip {file} -d {dictionary['fast_out_path']}/unzipped")
+        
     return
     
 def check_fast_qc(dictionary):
     #Parse and check for bad samples
     failed = []
-    os.chdir('unzipped')
-
-    for report in os.listdir():
+    
+    for report in os.listdir('unzipped'):
         if report.startswith("SRR"):
             os.chdir(report)
             with open('summary.txt') as f:
@@ -56,7 +69,6 @@ def check_fast_qc(dictionary):
     return failed
 
 def cutadapt(dictionary,subset):
-    os.chdir('cutadapt')
 
     for sample in subset:
         s1 = sample.replace("_2.","_1.")
@@ -65,36 +77,33 @@ def cutadapt(dictionary,subset):
         s2 = sample
         f2 = s2.split('/')[-1]
 
-        command1 = f"cutadapt -j 4 -a {dictionary['adapter_sequence']} -o {f1} ../../../{s1}"
-        command2 = f"cutadapt -j 4 -a {dictionary['adapter_sequence']} -o {f2} ../../../{s2}"
+        command1 = f"cutadapt -j 4 -a {dictionary['adapter_sequence']} -o data/cutadapt/{f1} {s1}"
+        command2 = f"cutadapt -j 4 -a {dictionary['adapter_sequence']} -o data/cutadapt/{f2} {s2}"
 
         os.system(command1)
         os.system(command2)
         
-    os.chdir('..')
     return
 
 def kallisto(dictionary,subset):
-    os.chdir('kallisto')
     
     for sample in subset:
         s1 = sample.replace("_2.","_1.")    
         s2 = sample
 
-        outpath = s1.split('/')[-1].split('_')[0]
+        outpath = "data/kallisto/" + s1.split('/')[-1].split('_')[0]
 
         if not os.path.isdir(outpath):
             os.system('mkdir ' + outpath)
 
-        command = f"/opt/kallisto_linux-v0.42.4/kallisto quant -i {dictionary['idx']} -o {outpath} -b {dictionary['num_bootstraps']} ../../../{s1} ../../../{s2}"
+        command = f"/opt/kallisto_linux-v0.42.4/kallisto quant -i {dictionary['idx']} -o {outpath} -b {dictionary['num_bootstraps']} {s1} {s2}"
 
         os.system(command)
-    os.chdir('..')
     return
     
 def gene_counts(dictionary,subset):
     
-    tsvs = glob.glob("kallisto/SRR*/*.tsv")
+    tsvs = glob.glob("data/kallisto/SRR*/*.tsv")
     print(tsvs)
     gene_counts = pd.read_csv(tsvs[0],sep='\t')
     gene_counts = gene_counts[['target_id','est_counts']].rename(columns={'est_counts': tsvs[0].split('/')[-2]})
@@ -104,5 +113,5 @@ def gene_counts(dictionary,subset):
         df = df[['est_counts','target_id']].rename(columns={'est_counts': tsv.split('/')[-2]})
         gene_counts = gene_counts.join(df.set_index('target_id'), on='target_id')
         
-    gene_counts.to_csv('kallisto/gene_counts.csv',index=False)
+    gene_counts.to_csv('data/gene_counts.csv',index=False)
     return
